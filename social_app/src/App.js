7bb1f1088da9c9cdcfb053362c2dd88b4defe8a5
NguyenNgocBaoITITIU19084 <ititiu19084@student.hcmiu.edu.vn>
2024-03-11 15:25:30 +0700

    setup project

---
+++ social_app/src/App.js
@@ -0,0 +1,12 @@
+import Home from "./pages/home/Home";
+import DeleteIcon from "@mui/icons-material/Delete";
+function App() {
+  return (
+    <div>
+      <Home />
+      <DeleteIcon />
+    </div>
+  );
+}
+
+export default App;
---
+++ social_app/src/pages/home/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function home() {
+  return <div>this is home page!</div>;
+}
+
+export default home;
